6e7b8b09ffe487fd31e2ba3774a6df073376da13
kladafox <klapatok1980@gmail.com>
2024-08-28 17:05:40 +0200

    Adjust rspec setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,10 +14,9 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
   config.filter_run_when_matching :focus
-  config.example_status_persistence_file_path = "spec/examples.txt"
   config.disable_monkey_patching!
   config.warnings = true
-  config.default_formatter = "doc" if config.files_to_run.one?
+  config.default_formatter = "progress" if config.files_to_run.one?
   config.order = :random
   Kernel.srand config.seed
 end
